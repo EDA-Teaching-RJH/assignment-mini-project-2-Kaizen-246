ca84f34bc032c634dab10bd7498eae1841db62d6
Kaizen-246 <kc101@kent.ac.uk>
2026-03-26 23:32:12 +0000

    Implement Validators and test

--- project/main.py
+++ project/main.py
@@ -1,30 +1,8 @@
-print ("LabLog Manager starting...")
+from lablog.validators import is_valid_email
 
-from lablog.models import LogEntry 
-entry = LogEntry("EXP-001", "2026-03-17", "test@email.com", "EQ-1", "Test")
-entry.display()
+email = input("Enter email: ")
 
-from lablog.models import ExperimentEntry, FaultEntry
-
-exp = ExperimentEntry(
-    "EXP-001",
-    "2026-03-17",
-    "test@email.com",
-    "EQ-AB123",
-    "Voltage test",
-    10,
-    2
-)
-
-fault = FaultEntry(
-    "FLT-001",
-    "2026-03-17",
-    "fault@email.com",
-    "EQ-XY999",
-    "Motor failure",
-    "high",
-    False
-)
-
-print("Experiment Power:", exp.power())
-fault.display()
+if is_valid_email(email):
+    print("Valid email")
+else:
+    print("Invalid email")
